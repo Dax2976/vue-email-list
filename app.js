51d5b77f4c209e003797ae439aa88173fed629f7
Dax2976 <ferrodavide55@gmail.com>
2022-04-29 15:14:56 +0200

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const app = new Vue({
     el:'#app',
-    data:{},
-    methods(){
-        
+    data:{
+
+        listEmail: [],
+    },
+    methods: {},
+    mounted(){
+        for(i=0;i<10;i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+                this.listEmail.push(response.data.response)
+            })
+        }
     }
 })
